3aba79cea6c934e3b8121d10eb5367db39e9cb82
Teresa Ng <teresa.ng@starlingbank.com>
2018-03-25 16:02:25 +0100

    Date formatting

--- app/src/main/java/com/novembergave/popularmovies/DetailActivity.java
+++ app/src/main/java/com/novembergave/popularmovies/DetailActivity.java
@@ -6,12 +6,16 @@ import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.format.DateUtils;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.novembergave.popularmovies.POJO.Movie;
 import com.squareup.picasso.Picasso;
+
+import org.threeten.bp.ZoneId;
+import org.threeten.bp.ZonedDateTime;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -58,12 +62,28 @@ public class DetailActivity extends AppCompatActivity {
     Movie movie = (Movie) getIntent().getSerializableExtra(EXTRA_MOVIE);
     // set title
     layout.setTitle(movie.getTitle());
-    // populate the image
+    // populate the image in both app bar and poster image
     Picasso.with(this).load("http://image.tmdb.org/t/p/w185/" + movie.getPosterPath()).into(displayImage);
     // populate the overview
     overViewText.setText(movie.getOverview());
-    // add the release date
-    date.setText(getString(R.string.release_date, movie.getReleaseDate()));
+    String releaseDate = movie.getReleaseDate();
+    if (releaseDate != null && !releaseDate.isEmpty()) {
+      // format the release date by first splitting the string at the dash
+      String[] strings = releaseDate.split("-");
+      // we always expect strings array to contain 3 items - year, month, day
+      if (strings.length == 3) {
+        // format to localised date string
+        ZonedDateTime dateTime = ZonedDateTime.of(Integer.valueOf(strings[0]),
+            Integer.valueOf(strings[1]), Integer.valueOf(strings[2]), 1, 1, 1, 1, ZoneId.systemDefault());
+        int flags = DateUtils.FORMAT_SHOW_YEAR;
+        String displayDate = DateUtils.formatDateTime(this, dateTime.toInstant().toEpochMilli(), flags);
+        date.setText(displayDate);
+      }
+    }
+    // if nothing has been set fallback without splitting or formatting
+    if (date.getText().toString().isEmpty()) {
+      date.setText(releaseDate);
+    }
 
     // display the rating - returns on the scale of 10, so we'll need to divide to show scale of 5
     float averageVote = (float) movie.getAverageVote() / 2;
